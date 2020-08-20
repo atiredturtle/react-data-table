aea486fff945a6acd6c82fc5e385a6fdd584c92d
atiredturtle <guy@segev.com>
2020-08-20 20:54:20 +1000

    adds resizing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Table from "./Table";
+
+const data = [
+  ["first name", "last name", "age", "height (m)"],
+  ["Guy", "Segev", "22", "186"],
+  ["Tim", "Horton", "25", "220"],
+  ["Joseph", "Green", "18", "165"],
+];
 
 function App() {
-  return <div>Testing</div>;
+  return <Table rows={data} />;
 }
 
 export default App;
--- src/Table/Table.tsx
+++ src/Table/Table.tsx
@@ -1,6 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
+import { Resizable, ResizableBox, ResizeCallbackData } from "react-resizable";
+import "react-resizable/css/styles.css";
 
-type TableProps = {};
-export const Table = (props: TableProps) => {
-  return <div>Hello</div>;
+function DragHandle() {
+  return (
+    <span
+      style={{
+        cursor: "col-resize",
+        width: "10px",
+        float: "right",
+        height: "100%",
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <div
+        style={{
+          width: "2px",
+          background: "black",
+        }}
+      >
+        {" "}
+      </div>
+    </span>
+  );
+}
+
+type TableProps = {
+  rows: string[][];
 };
+const borderStyle = { border: "0px solid black" };
+export function Table(props: TableProps) {
+  const { rows } = props;
+  const defaultColWidth = 200;
+  const [columnWidths, setColumnWidths] = useState(
+    rows[0].map(() => defaultColWidth)
+  );
+
+  const resizeColumn = (columnIndex: number, width: number) => {
+    setColumnWidths(
+      columnWidths.map((v, ind) => (ind === columnIndex ? width : v))
+    );
+  };
+
+  return (
+    <table style={{ ...borderStyle }}>
+      {rows.map((row) => (
+        <tr style={{ ...borderStyle }}>
+          {row.map((col, colInd) => (
+            // <ResizableBox width={200} height={20}>
+            <td style={{ ...borderStyle, width: columnWidths[colInd] }}>
+              <ResizableBox
+                width={columnWidths[colInd]}
+                resizeHandles={["e"]}
+                handle={DragHandle}
+                height={30}
+                onResize={(_, data: ResizeCallbackData) => {
+                  console.log("data", data.size);
+                  resizeColumn(colInd, data.size.width);
+                }}
+              >
+                {col}
+                {/*  <span style={{ width: "100%", background: "red" }}>{col}</span>*/}
+              </ResizableBox>
+            </td>
+            // </ResizableBox>
+          ))}
+        </tr>
+      ))}
+    </table>
+  );
+}
+
+/* Options for data design
+ * 1. columns = string[], rows = {[key: columns]: any}
+ * 2. We can let them do it...
+ *  > They provide columns
+ */
